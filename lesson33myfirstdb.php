a91ad967f43071744ef381cda79a2568002b3559
Ko Tha <datalandtechnology@gmail.com>
2020-11-28 20:09:03 +0630

    28 / Nov / 2020

--- lesson33myfirstdb.php
+++ lesson33myfirstdb.php
@@ -6,14 +6,58 @@ define("DB_PASS","");
 define("DB_NAME","dbone");
 
 //Connection to DB and check error
-    //mysqli_connect(host,username,password,dbname);
+        //mysqli_connect(host,username,password,dbname);
 function dbconnection(){
     $db = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
-    echo mysqli_connect_errno() > 0 ? "Connection Error " : "Connection Success";
+        // echo mysqli_connect_errno() > 0 ? "Connection Error " : "Connection Success";
+    if(mysqli_connect_errno() > 0){
+        // echo "Connection Error";
+        die("Connection Filed");
+    }else{
+        return $db;
+    }
 }
 
 
-dbconnection();
+
+//2 Query ( Table )
+$query = "SELECT * FROM users";
+
+
+function executequery($qry){
+
+//1 Database ( Successful ) 
+$database = dbconnection();
+
+//3 Connect to Database adn Query
+        // mysqli_query("database","query");
+$results = mysqli_query($database,$qry);
+
+if(mysqli_num_rows($results) > 0){
+    foreach($results as $result){
+        // echo $result["email"]. "<br/>";
+        foreach($result as $key=>$value){
+            echo "Key is = ${key} " . " Value is = ${value}" . "<br/>";
+       }
+    }
+}else{
+    echo "No Date";
+}
+
+}
+
+
+executequery($query);
+
+
+
+
+
+
+
+
+      
+   
 
 
 
---
+++ lesson34arrayfunction.php
@@ -0,0 +1,33 @@
+<?php 
+// Array Function
+
+$arr = ["orange","red","blue","green","black"];
+
+// beautyprint($arr);
+
+// echo count($arr);
+
+// array_push($arr,"yellow","white","gray");
+// beautyprint($arr);
+
+// array_pop($arr);
+// beautyprint($arr);
+
+// array_shift($arr);
+// beautyprint($arr);
+
+
+// array_unshift($arr,"yellow","white","gray");
+// beautyprint($arr);
+
+
+
+
+
+
+
+function beautyprint($arr){
+echo "<pre>".print_r($arr,true)."</pre>";
+}
+
+?>
