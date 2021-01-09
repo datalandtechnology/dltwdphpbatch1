e422670020820113337b2c2a86520178d35eee5d
Ko Tha <datalandtechnology@gmail.com>
2021-01-09 19:25:14 +0630

    9 / Jan / 2021

--- oop/mysqlioopintro/app/database/class_db.php
+++ oop/mysqlioopintro/app/database/class_db.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace app\database;
 class class_db{
     const DB_HOST = "localhost";
@@ -31,11 +30,83 @@ class class_db{
         $stmt = $this->database->prepare("SELECT * FROM users WHERE id=?");
         $stmt->bind_param("i",$id);
         $stmt->execute();
-        $stmt->bind_result($id,$email,$password,$created_at);
+        $stmt->bind_result($id,$email,$password,$status_id,$created_at);
 
         while($stmt->fetch()){
             echo "this is single user , id is = ${id} and email is = ${email} " . "<hr/>";
         }
+    }
+
+    public function getusersbystatus($status){
+        $stmt = $this->database->prepare("SELECT * FROM users WHERE status_id=?");
+        $stmt->bind_param("i",$status);
+        $stmt->execute();
+        $stmt->bind_result($id,$email,$password,$status_id,$created_at);
+
+        while($stmt->fetch()){
+            echo "this is ${status} user , id is = ${id} and email is = ${email} " . "<hr/>";
+        }
+
+    }
+
+    public function insertsguser($email,$password,$status_id){
+        $crtdate = date("Y-m-d");
+//        echo $crtdate;
+        $stmt = $this->database->prepare("INSERT INTO users (email,password,status_id,created_at) VALUES (?,?,?,?)");
+        $stmt->bind_param("ssis",$email,$password,$status_id,$crtdate);
+        $result = $stmt->execute();
+        $lstinsertid = $stmt->insert_id;
+
+        if($result){
+            echo "Data Insert Successfully = id by ${lstinsertid}";
+        }else{
+            echo "Insert Failed";
+        }
+
+    }
+
+    public function insertmultiusers($users){
+        $crtdate = date("Y-m-d");
+        $stmt = $this->database->prepare("INSERT INTO users (email,password,status_id,created_at) VALUES (?,?,?,?)");
+
+        foreach ($users as $user){
+            $stmt->bind_param("ssis", $user[0],$user[1],$user[2],$crtdate);
+            $result = $stmt->execute();
+            $lstinsertid = $stmt->insert_id;
+
+                if($result){
+                    echo "Data Insert Successfully = id ${lstinsertid}" . "<br/>";
+                }else{
+                    echo "Insert Failed". "<br/>";
+            }
+        }
+
+    }
+
+
+    public function updateuser($id,$status){
+        $stmt = $this->database->prepare("UPDATE users SET status_id=? WHERE id=?");
+        $stmt->bind_param("ii",$status,$id);
+        $result = $stmt->execute();
+        if($result){
+            echo "Update Insert Successfully = id by ${id}" . "<br/>";
+        }else{
+            echo "Update Failed". "<br/>";
+        }
+
+    }
+
+    public function deleteuser($id){
+        $stmt = $this->database->prepare("DELETE FROM users WHERE id=?");
+        $stmt->bind_param("i",$id);
+        $result = $stmt->execute();
+
+        if($result){
+            echo "Delete Data Successfully = id by ${id}" . "<br/>";
+        }else{
+            echo "Delete Failed". "<br/>";
+        }
+
     }
 
 }
--- oop/mysqlioopintro/index.php
+++ oop/mysqlioopintro/index.php
@@ -15,6 +15,31 @@ class index{
         $this->db->getallusers();
 
         $this->db->getsguser(3);
+
+
+        $this->db->getusersbystatus(2);
+
+//        $this->db->insertsguser("coco@gmail.com","123456",1);
+
+
+        $users = [
+          ["yoyo@gmail.com","123456",1],
+          ["popo@gmail.com","123456",1],
+          ["roro@gmail.com","123456",1],
+          ["vovo@gmail.com","123456",1],
+          ["bobo@gmail.com","123456",1],
+          ["zozo@gmail.com","123456",1],
+        ];
+
+//        $this->db->insertmultiusers($users);
+
+
+//        $this->db->updateuser(38,2);
+
+
+        $this->db->deleteuser(3);
+
+
     }
 }
 
